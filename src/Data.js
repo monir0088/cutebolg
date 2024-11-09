9726972e44253254daacef8060f751b9c19e4246
ALI NUR <alinur.bd818@gmail.com>
2024-11-10 01:22:42 +0600

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+const datas = [
+  {
+    name: 'data',
+    title: 'Cute Blog 🥳',
+    description:
+      'New and Free template download themes website blog for react.',
+    intro: 'Read Documentation →',
+    img: 'https://mesinkasironline.web.app/img/createwebsiteusingreact.png',
+  },
+];
+
+export default datas;
